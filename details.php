e775dc085ca3fa148f4f19cd8719febd52ccfa83
Daniel Ezekiel <dbezekiel77@gmail.com>
2025-07-22 15:34:38 +0100

    feat: added delete feature to the details page for each album

--- details.php
+++ details.php
@@ -21,6 +21,8 @@
         if(isset($_GET["id"])){
             // Grabbing the album id from the GET query parameter
             $album_id = $_GET["id"];
+            $album_title;
+            $artist_name;
             
             // Query to selectthe appropriate ablum using the album id from the GET parameter
             $sql_album = "SELECT albums.Title as AlbumTitle, artists.Name as ArtistName from albums JOIN artists ON albums.ArtistId = artists.ArtistId WHERE albums.AlbumId = $album_id";
@@ -29,25 +31,45 @@
 
             $album_details = $conn->query($sql_album);
             $tracks = $conn->query($sql_tracks);
+
+            // From the database query response for album details, update the values for the album_title and artist_name variables
+            while($album_detail = $album_details->fetch_assoc()){
+                $album_title = $album_detail["AlbumTitle"];
+                $artist_name = $album_detail["ArtistName"];
+            }
+        }
+
+        // PHP Code to handle album deletion
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            // Getting the album id to setup the deletion task
+            $album_id = $_POST["AlbumId"];
+
+            // query to delete album
+            $sql_delete_album = "DELETE FROM albums WHERE AlbumId = $album_id";
+            // query to delete tracks for the selected album
+            $sql_delete_albumTracks = "DELETE FROM tracks WHERE AlbumId = $album_id";
+
+            $conn->query($sql_delete_album);
+            $conn->query($sql_delete_albumTracks);
+
+            // Redirect to the page containing the deleted album
+            header("Location: /project");
         }
     ?>
 
     <header>
         <h1 class="gradient-bg1 gradient-text-color-support">.Chinook Music Store.</h1>
-        <?php 
-                // From the database query response, display the album title and artist name
-                while($album_detail = $album_details->fetch_assoc()){
-                    echo "<h2 class=\"gradient-bg2 gradient-text-color-support\">";
-                        echo "<span>Album Name</span>";
-                        echo "<span>" . $album_detail["AlbumTitle"] . "</span>";
-                    echo "</h2>";
+
+        <h2 class="gradient-bg2 gradient-text-color-support">
+            <span>Album Name</span>
+            <span><?php echo "$album_title" ?></span>
+        </h2>
                         
-                    echo "<h3 class=\"gradient-bg2 gradient-text-color-support\">";
-                        echo "<span>Artist</span>";
-                        echo "<span>" . $album_detail["ArtistName"] . "</span>";
-                    echo "</h3>";
-                }
-        ?>        
+        <h3 class="gradient-bg2 gradient-text-color-support">
+            <span>Artist</span>
+            <span><?php echo "$artist_name" ?></span>
+        </h3>
+                        
     </header>
 
     <main>
@@ -59,11 +81,13 @@
                 </a>
             </div>
 
-            <div class="update-btn">
+            <div class="page-actions">
                 <?php
                     // Handle routing to the page for updating album details
                     echo "<a class=\"gradient-bg3\" href=\"/project/insert-album.php?id=" . $album_id . "\">";
                     echo "Update Album</a>";
+
+                    echo "<button type=\"button\" class=\"action delete-btn\" data-album-Id=\"$album_id\" data-album-Title=\"$album_title\" data-artist-Name=\"$artist_name\">Delete Album</button>";
                 ?>
             </div>
         </div>
@@ -120,7 +144,30 @@
                 </table>
             </div>
         </div>
+
+        <!-- Delete modal -->
+        <div class="modal_overlay">
+            <div class="modal">
+                <h5>Delete Album</h5>
+
+                <p>Are you sure you want to delete Album <span class="gradient-bg1 gradient-text-color-support modal-info id">25</span>: <span class="gradient-bg1 gradient-text-color-support modal-info title">Album Title</span> by <span class="gradient-bg1 gradient-text-color-support modal-info artist-name">Artist name</span> including <span class="gradient-bg1 gradient-text-color-support modal-info">all its tracks</span>?</p>
+
+                <form method="post" id="delete-form">
+                    <button type="button" class="modal-btn cancel-btn">Cancel</button>
+                    <button type=submit class="modal-btn delete-btn">Delete</button>
+                    
+                    <input class="modal-form album-id" type="hidden" name="AlbumId">
+                    <input class="modal-form album-title" type="hidden" name="AlbumTitle">
+                    <input class="modal-form artist-name" type="hidden" name="ArtistName">
+                </form>
+            </div>
+        </div>
     </main>
-    <?php $conn->close() ?>
+
+    <script defer src="/project/js/script.js"></script>
+    <?php 
+        // Closing the connection
+        $conn->close(); 
+    ?>
 </body>
 </html>
